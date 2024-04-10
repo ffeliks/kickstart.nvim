b4c4ff92e3180e3b51e28c40352cb16ec8041ac3
Feliks Toomsoo <feliks.toomsoo@gmail.com>
2024-04-10 20:30:11 +0300

    Display hidden files in neotree

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -12,6 +12,9 @@ return {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+        },
       },
     }
 
